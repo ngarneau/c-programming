4e8cb3510de4d80d5e87db8625bfeb1c4f99fb9d
Nicolas Garneau <ngarneau@ngarneau.com>
2012-10-27 13:04:11 -0400

    First and second example

---
+++ hello/hello.c
@@ -0,0 +1,16 @@
+/************************
+ * hello -- program to print out "hello world".
+ *
+ * author: Nicolas Garneau
+ * purpose: Learning C
+ * usage: runs the program and the message appears.
+ *
+ ************************/
+#include <stdio.h>
+
+int main()
+{
+    /* Tell the world hello */
+    printf("Hello world\n");
+    return 0;
+}
